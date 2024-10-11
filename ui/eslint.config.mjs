5c21a3461a3d092f5df9ecdabad64440d60225a0
joyanto-bhakto <joyanto@myuxpro.com>
2024-10-12 03:19:07 +0600

    Completed task
    (cherry picked from commit 72d2dc23df9921fdc648b88a5aef0e34180b724b)

--- ui/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import withNuxt from "./eslint.config.mjs";
-
-export default withNuxt({
-  files: ["ui/**"],
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": "error",
-  },
-});
